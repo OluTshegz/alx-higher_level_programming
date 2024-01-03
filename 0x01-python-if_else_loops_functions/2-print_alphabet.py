ff8fb3ce42954898bcfaa49e2f7314b8e8fbdaa3
Olusegun <ayosegun2001@gmail.com>
2024-01-03 19:30:56 +0100

    Task 2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game: Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """prints the ASCII alphabet, in lowercase, not followed by a new line."""
 for alphabet in range(97, 123):
-	print(f"{alphabet:c}", end="")
+    print("{alphabet:c}".format(alphabet), end="")
