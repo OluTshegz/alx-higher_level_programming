adfd6ae739f80adf1c65cd544ebcd79145a7121a
Olusegun <ayosegun2001@gmail.com>
2024-01-04 22:21:42 +0100

    Task 8. ByteCode -> Python #3: Write the Python function def magic_calculation(a, b):

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Match bytecode"""
-# from magic_calculation_102 import add, sub
+"""from magic_calculation_102 import add, sub"""
 
 
 def magic_calculation(a, b):
@@ -10,6 +10,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return c
+        return c
     else:
         return sub(a, b)
