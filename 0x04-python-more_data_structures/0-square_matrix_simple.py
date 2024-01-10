b3fd7ea4ec01eae3db5879b2f32d506edad3e6d4
Olusegun <ayosegun2001@gmail.com>
2024-01-10 13:43:15 +0100

    Task 0. Squared simple: Write a function that computes the square value of all integers of a matrix.

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is not None:
+        new_list = []
+        for row in matrix:
+            row_list = []
+            for element in row:
+                row_list.append(element ** 2)
+            new_list.append(row_list)
+        return new_list
+    else:
+        return matrix
