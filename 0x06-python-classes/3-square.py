d2050c03e147e461f298596c857c9d8cdeeb2ae3
Olusegun <ayosegun2001@gmail.com>
2024-01-23 19:08:04 +0100

    Task 3. Area of a square: Write a class Square that defines a square by: (based on 2-square.py), Private instance attribute: size, Instantiation with optional size: def __init__(self, size=0):, Public instance method: def area(self): that returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""defines a class 'Square'"""
+
+
+class Square:
+    """represents a square blueprint"""
+    def __init__(self, size=0):
+        """initializes a new square
+        Args:
+            size(int): size of the new square
+        """
+        # size must be an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        # it should be greater than or equal to zero
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        # set the size of the Square if all goes well
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
