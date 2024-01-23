9c09503cd3c5b99ad1f0fa898e8b072583f78614
Olusegun <ayosegun2001@gmail.com>
2024-01-23 19:39:19 +0100

    Task 4. Access and update private attribute: Write a class Square that defines a square by: (based on 3-square.py), Private instance attribute: size, property def size(self): to retrieve it, property setter def size(self, value): to set it:, Instantiation with optional size: def __init__(self, size=0):, Public instance method: def area(self): that returns the current square area

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""defines a class 'Square'"""
+
+
+class Square:
+    """represents a square blueprint"""
+    def __init__(self, size=0):
+        """initializes a new square
+        Args:
+            size(int): size of the new square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """gets the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        # value must be an integer
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        # it should be greater than or equal to zero
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        # set the value of the Square if all goes well
+        self.__size = value
+
+    def area(self):
+        """returns the area of the square"""
+        return (self.__size * self.__size)
