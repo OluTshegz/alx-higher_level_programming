d4cca472c35ce33233bb1895690205988b079e96
Olusegun <ayosegun2001@gmail.com>
2024-01-24 19:57:37 +0100

    Task 8. Print Square instance: Write a class Square that defines a square by: (based on 6-square.py), Printing a Square instance should have the same behavior as my_print()

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -11,16 +11,19 @@ class Square:
 
         Args:
             size(int): size of the square
+            position(int): position of the square
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """gets the size of the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """sets the size of the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -29,10 +32,12 @@ class Square:
 
     @property
     def position(self):
+        """gets the position of the square"""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """sets the position of the square"""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 any(type(num) is not int or num < 0 for num in value)):
@@ -40,9 +45,11 @@ class Square:
         self.__position = value
 
     def area(self):
+        """Return the current area of the square"""
         return (self.__size ** 2)
 
     def my_print(self):
+        """Print the square with the # character"""
         if self.__size == 0:
             print()
             return
@@ -58,6 +65,7 @@ class Square:
             print()
 
     def __str__(self):
+        """Printing a Square instance similar to my_print()"""
         if self.__size != 0:
             for x in range(self.__position[1]):
                 print()
