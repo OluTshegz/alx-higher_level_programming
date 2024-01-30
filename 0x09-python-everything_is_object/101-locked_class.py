03e5e7a59df4637217ecab2c951c913b6bf51a66
Olusegun <ayosegun2001@gmail.com>
2024-01-30 15:44:07 +0100

    Task 30. Low memory cost: Write a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """a class LockedClass with no class or object attribute,
-	that prevents the user from dynamically creating
-	new instance attributes, except if the
-	new instance attribute is called first_name.
+    that prevents the user from dynamically creating
+    new instance attributes, except if the
+    new instance attribute is called first_name.
 """
 
 
 class LockedClass:
-	__slots__ = ["first_name"]
+    __slots__ = ["first_name"]
