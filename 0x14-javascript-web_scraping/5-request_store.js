ac32e0b07345c6d232d32e0dbf98184f98540987
Olusegun <ayosegun2001@gmail.com>
2024-05-21 17:18:32 +0100

    0x14. JavaScript - Web scraping: Scripting, API, JavaScript

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+if (process.argv.length < 4) {
+  console.error('Usage: ./5-request_store.js <URL> <file>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+    return;
+  }
+
+  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+    if (err) {
+      console.error(`Error writing to file: ${err}`);
+    } else {
+      console.log(`The content of ${url} is saved to ${filePath}`);
+    }
+  });
+});
