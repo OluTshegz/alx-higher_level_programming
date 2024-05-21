85d5d4440c6bcbc8c5b389336b3561a86eef6c5c
Olusegun <ayosegun2001@gmail.com>
2024-05-21 16:58:16 +0100

    0x14. JavaScript - Web scraping: Scripting, API, JavaScript

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node 4-starwars_count.js <api_url>');
+  process.exit(1);
+}
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+let count = 0;
+
+const fetchFilms = (url) => {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+      console.error(`Error: ${response.statusCode}`);
+      process.exit(1);
+    }
+
+    const data = JSON.parse(body);
+    data.results.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    });
+
+    if (data.next) {
+      fetchFilms(data.next);
+    } else {
+      console.log(count);
+    }
+  });
+};
+
+fetchFilms(apiUrl);
